8ebe2bebc79a3d469d7c81fee57e4176b2252167
Sushama <sushamajarange@gmail.com>
2022-08-29 18:32:16 +0800

    JavaScript Programs added

---
+++ JavaScript/dateEx.js
@@ -0,0 +1,20 @@
+//mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth() + 1;
+var yyyy = today.getFullYear();
+if (dd < 10) {
+    dd = '0' + dd;
+}
+if (mm < 10) {
+    mm = '0' + mm;
+}
+console.log(today);
+today = mm + '-' + dd + '-' + yyyy;
+console.log(today);
+today = mm + '/' + dd + '/' + yyyy;
+console.log(today);
+today = dd + '-' + mm + '-' + yyyy;
+console.log(today);
+today = dd + '/' + mm + '/' + yyyy;
+console.log(today);
